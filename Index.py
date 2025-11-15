fd8609adc6d8bfba6eeaff1f90894cfd75fb6db2
Arialyson Lima <arialyson@gmail.com>
2025-11-15 09:58:32 -0300

    2.0
    Correções, organização do código

--- Index.py
+++ Index.py
@@ -35,19 +35,24 @@ while True:
             deposito = float(input("Valor a ser depositado R$"))
             saldo += deposito
             numero_de_depositos += 1
-            print(f"Você depositou R${deposito:.2f}")
+            print(f"\u2714 Você depositou R${deposito:.2f}")
+            print()
             input('Pressione ENTER para continuar...')
         case 2:
             saque = float(input("Quanto quer sacar? R$"))
             saldo -= saque
             numero_de_saques += 1
-            print(f"Você sacou R${saque:.2f}")
+            print(f"\u2714 Você sacou R${saque:.2f}")
+            print()
             input('Pressione ENTER para continuar...')
         case 3:
             print("=====EXTRATO=====")
+            print()
             print(f"Seu saldo é de R${saldo:.2f}")
+            print()
             print(f"Você fez {numero_de_depositos} Depositos")
             print(f"você fez {numero_de_saques} saques")
+            print()
             print("=================")
             input('pressione ENTER para continuar...')
         case 4:
